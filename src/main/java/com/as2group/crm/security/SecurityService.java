059db23d017a47e6eb4f82958ff88e40d7ff66c3
Kauã Borgarelli <borgarellitavares@outlook.com>
2023-11-17 20:24:23 -0300

    login

--- src/main/java/com/as2group/crm/security/SecurityService.java
+++ src/main/java/com/as2group/crm/security/SecurityService.java
@@ -3,12 +3,14 @@ package com.as2group.crm.security;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.as2group.crm.enumeration.EmployeeStatus;
 import com.as2group.crm.model.Employee;
 import com.as2group.crm.repository.EmployeeRepository;
 import com.as2group.crm.util.RoleUtil;
@@ -25,7 +27,13 @@ public class SecurityService implements UserDetailsService{
         if(employeeOp == null) {
             throw new UsernameNotFoundException("Employee not found");
         }
+
         Employee employee = employeeOp.get();
+
+        if (employee.getStatus() == EmployeeStatus.INATIVO) {
+        throw new DisabledException("Employee is inactive");
+    }
+
         return User.builder().username(employee.getEmail()).password(employee.getPassword()).authorities(RoleUtil.parseAuthority(employee.getRoles())).build();
     }
 
